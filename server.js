162b74f0f3245a0b882617ebb2baf9c1099876ea
Menna94 <menna.mooc94@gmail.com>
2021-12-23 11:39:51 +0200

    cors

--- server.js
+++ server.js
@@ -11,7 +11,18 @@ const RecipientRoutes = require('./Routes/recipient.routes');
 const DonerRoutes = require('./Routes/doner.routes');
 
 //configs
-app.use(express.json())
+app.use(express.json());
+
+//cors
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    )
+    next()
+})
 
 // mount routes
 app.use('/api/v1/recipients', RecipientRoutes);
